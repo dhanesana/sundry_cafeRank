948692ef079b4432f7d47ad24d95e485f7352ba6
David P. Hanesana <dhanesana@gmail.com>
2016-02-20 22:34:31 -0800

    Generate Cafe model and migration

---
+++ db/migrate/20160221063415_create_caves.rb
@@ -0,0 +1,13 @@
+class CreateCaves < ActiveRecord::Migration
+  def change
+    create_table :caves do |t|
+      t.string :kor_name
+      t.string :eng_name
+      t.string :url
+      t.float :members
+      t.string :type
+
+      t.timestamps null: false
+    end
+  end
+end
